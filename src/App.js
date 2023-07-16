d3d25ba1d250ce2d86dbd179f53762bb99548ecd
Anu-zxy22 <125558451+Anu-zxy22@users.noreply.github.com>
2023-07-16 20:36:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import './App.css';
+import Login from './components/login';
+import Signup from './components/signup';
+import NavBar from './components/home';
+import Sidepan from './components/sidepan';
+import Tandc from './components/tandc';
+import Policy from './components/policy';
+import Faq from './components/faq';
+import Service from './components/service';
+import Task from './components/task';
+import Schedule from './components/schedule';
+import Progress from './components/progress';
+import Job from './components/job';
+import Inventory from './components/inventory';
+import Expense from './components/expense';
+import Emergency from './components/emergency';
+import Security from './components/security';
+
+
+
+
+
+
+function App() {
+  return (
+    <Router>
+   
+    
+      <Routes>
+      
+      <Route path='/' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/home' element={<NavBar/>}/>
+      <Route path='/sidepan' element={<Sidepan/>}/>
+      <Route path='/tandc' element={<Tandc/>}/>
+      <Route path='/policy' element={<Policy/>}/>
+      <Route path='/faq' element={<Faq/>}/>
+      <Route path='/service' element={<Service/>}/>
+      <Route path='/task' element={<Task/>}/>
+      <Route path='/schedule' element={<Schedule/>}/>
+      <Route path='/progress' element={<Progress/>}/>
+      <Route path='/job' element={<Job/>}/>
+      <Route path='/inventory' element={<Inventory/>}/>
+      <Route path='/expense' element={<Expense/>}/>
+      <Route path='/emergency' element={<Emergency/>}/>
+      <Route path='/security' element={<Security/>}/>
+
+      
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App
