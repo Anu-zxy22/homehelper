659bb0d1a256b9aa43a476b54d33f25e77dafd60
Anu-zxy22 <125558451+Anu-zxy22@users.noreply.github.com>
2023-07-17 15:47:41 +0530

    Add files via upload

---
+++ Day 7/src/components/service.js
@@ -0,0 +1,69 @@
+import * as React from 'react';
+// import Card from '@mui/material/Card';
+// import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
+// import Typography from '@mui/material/Typography';
+// import { Button, CardActionArea, CardActions } from '@mui/material';
+import './service.css';
+
+export default function MultiActionAreaCard() {
+  return (
+    
+    <div class="row">
+  <div class="column">bbb<br></br>
+    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAA1BMVEUBRCF+xM2yAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO8GxYgAAb0jQ/cAAAAASUVORK5CYII=" alt="Snow" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiWesDOkWa-AFfz7RS6P8vv3_F5XX6t9sJ4g&usqp=CAU" alt="Forest" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSbZYfMK0c0Qkjt3Pz92F8zYf1d3fcUBiM8A&usqp=CAU" alt="Mountains" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSbZYfMK0c0Qkjt3Pz92F8zYf1d3fcUBiM8A&usqp=CAU" alt="Mountains" style={{ width:"200px",height:"130px" }}/>
+  </div><div class="column"><br></br>
+    <img src="https://cdn-icons-png.flaticon.com/128/2767/2767192.png" alt="Mountains" style={{ width:"70px",height:"70px" }}/>
+  </div><br></br>
+
+
+  <div class="column">bbb
+    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAA1BMVEUBRCF+xM2yAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO8GxYgAAb0jQ/cAAAAASUVORK5CYII=" alt="Snow" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiWesDOkWa-AFfz7RS6P8vv3_F5XX6t9sJ4g&usqp=CAU" alt="Forest" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSbZYfMK0c0Qkjt3Pz92F8zYf1d3fcUBiM8A&usqp=CAU" alt="Mountains" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSbZYfMK0c0Qkjt3Pz92F8zYf1d3fcUBiM8A&usqp=CAU" alt="Mountains" style={{ width:"200px",height:"130px" }}/>
+  </div><br></br>
+
+  <div class="column">bbb
+    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAA1BMVEUBRCF+xM2yAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO8GxYgAAb0jQ/cAAAAASUVORK5CYII=" alt="Snow" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiWesDOkWa-AFfz7RS6P8vv3_F5XX6t9sJ4g&usqp=CAU" alt="Forest" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSbZYfMK0c0Qkjt3Pz92F8zYf1d3fcUBiM8A&usqp=CAU" alt="Mountains" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSbZYfMK0c0Qkjt3Pz92F8zYf1d3fcUBiM8A&usqp=CAU" alt="Mountains" style={{ width:"200px",height:"130px" }}/>
+  </div><br></br>
+
+  <div class="column"><br></br>
+    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACoCAMAAABt9SM9AAAAA1BMVEUBRCF+xM2yAAAAR0lEQVR4nO3BAQEAAACCIP+vbkhAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAO8GxYgAAb0jQ/cAAAAASUVORK5CYII=" alt="Snow" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiWesDOkWa-AFfz7RS6P8vv3_F5XX6t9sJ4g&usqp=CAU" alt="Forest" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSbZYfMK0c0Qkjt3Pz92F8zYf1d3fcUBiM8A&usqp=CAU" alt="Mountains" style={{ width:"200px",height:"130px" }}/>
+  </div>
+  <div class="column"><br></br>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSbZYfMK0c0Qkjt3Pz92F8zYf1d3fcUBiM8A&usqp=CAU" alt="Mountains" style={{ width:"200px",height:"130px" }}/>
+  </div>
+</div>
+  );
+}
